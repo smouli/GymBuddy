2ac9b20dcb497448a88d741d5190b032a42b328f
hlingare <harshalingareddy@pal-nat186-139-151.itap.purdue.edu>
2017-09-22 01:09:09 -0400

    change

--- GYMBUDDY/GYMBUDDY/TabBarHomePageViewController.swift
+++ GYMBUDDY/GYMBUDDY/TabBarHomePageViewController.swift
@@ -8,10 +8,38 @@
 
 import UIKit
 
-class TabBarHomePageViewController: UIViewController {
-
+class TabBarHomePageViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let list = ["Preset Workout 1", "Preset Workout 2", "Preset Workout 3", "Preset Workout 4", "Preset Workout 5"]
+    var myIndex = 0
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return(list.count)
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return (cell)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        myIndex = indexPath.row
+        performSegue(withIdentifier: "segue1", sender: self)
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        
 
         // Do any additional setup after loading the view.
     }
