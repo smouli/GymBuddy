a2d6198034ed515900cf3a9b1d687125cdbe3eb9
sanatmouli <sanatmouli@gmail.com>
2017-10-22 19:26:23 -0400

    added personalized excercise view controller

--- GYMBUDDY/GYMBUDDY/ViewController.swift
+++ GYMBUDDY/GYMBUDDY/ViewController.swift
@@ -7,12 +7,62 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var list = [String]()
+    var data = [String]()
+    var ref: DatabaseReference?
+    var databaseHandle: DatabaseHandle?
+
+    @IBAction func submit(_ sender: Any) {
+        dump(data)
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(list.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell_1")
+        cell.textLabel?.text = list[indexPath.row]
+        return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if (tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCellAccessoryType.checkmark){
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.none
+            if (data.contains(list[indexPath.row])){
+                data.remove(at: data.index(of: list[indexPath.row])!)
+            }
+            
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.checkmark
+            if (!data.contains(list[indexPath.row])){
+                var i = 0
+                data.insert(list[indexPath.row], at: i)
+                i = i + 1
+            }
+        }
+        dump(data)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        ref = Database.database().reference()
+        databaseHandle = ref?.child("Exercises").observe(.childAdded, with: { (snapshot) in
+            let post = snapshot.value as? String
+            if let actualPost = post{
+                self.list.append(actualPost)
+                self.tableView.reloadData()
+            }
+
+        })
+        dump(list)
     }
 
     override func didReceiveMemoryWarning() {
