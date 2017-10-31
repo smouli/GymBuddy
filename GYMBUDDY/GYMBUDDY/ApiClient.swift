5b3258e7085a709b97f7879a9195845979baf2bb
hlingare <harshalingareddy@pal-nat186-129-203.itap.purdue.edu>
2017-10-31 01:48:06 -0400

    add features

--- GYMBUDDY/GYMBUDDY/ApiClient.swift
+++ GYMBUDDY/GYMBUDDY/ApiClient.swift
@@ -40,7 +40,7 @@ class ApiClient: BDBOAuth1RequestOperationManager {
         // For additional parameters, see http://www.yelp.com/developers/documentation/v2/search_api
         
         // Default the location to San Francisco
-        var parameters: [String : AnyObject] = ["term": term as AnyObject, "ll": "37.785771,-122.406165" as AnyObject]
+        var parameters: [String : AnyObject] = ["term": term as AnyObject, "location": "West Lafayette" as AnyObject]
         
         if sort != nil {
             parameters["sort"] = sort!.rawValue as AnyObject?
